47d2f80322babfb40537082527ace6f40f63c0bf
unknown <25ncornejo@ralstonschools.org>
2023-04-24 21:28:44 -0500

    final-hour-for-assessment

--- final-assessment/js/orange.js
+++ final-assessment/js/orange.js
@@ -1,5 +1,16 @@
-$(function() {
-    $("#exit").click(function() {
-        $("#schedule").toggleClass("popup");
+  $(document).ready(function() {
+        $("#contact").click(function(){
+            $("#contact-back").css("display", "block");
+        });
+
+        $("#exit-away").click(function(){
+            $("#contact-back").css("display", "none");
+        });
     });
-});
+
+    $(document).ready(function(){
+        $("#schedule").click(function(){
+            $("#popup").css("display", "block");
+        });
+        $("#exit-button").css("display", "none");
+    });
