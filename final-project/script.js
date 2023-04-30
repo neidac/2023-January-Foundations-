a77a4d88a5a8e4f05f1777254cdcf93ad16e1f43
unknown <25ncornejo@ralstonschools.org>
2023-04-30 14:22:10 -0500

    final-submit

--- final-project/script.js
+++ final-project/script.js
@@ -3,16 +3,16 @@ var questions = document.getElementsByClassName("questions");
 for(var i = 0; i < questions.length; i++){
     questions[i].addEventListener("click", function() {
         this.classList.toggle("active");
+        var chevron = this.firstElementChild;
 
+        chevron.classList.toggle("fa-chevron-down");
+        chevron.classList.toggle("fa-chevron-up");
+
+        
         var answers = this.nextElementSibling;
-        if (answers.style.display === "block") {
-            answers.style.display = "none";
-
-            (answers.style.maxHeight) 
+        if  (answers.style.maxHeight) {
             answers.style.maxHeight = null;
         } else {
-            answers.style.display = "block"
-
             answers.style.maxHeight = answers.scrollHeight + "px";
         }
     });
